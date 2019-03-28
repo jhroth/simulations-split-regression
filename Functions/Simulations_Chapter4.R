a14e0223a96b4641351f416ddeb280c6d5b17c7f
Jeremy Roth <jhroth@uw.edu>
2019-03-28 08:03:54 -0700

    updated function definitions

--- Functions/Simulations_Chapter4.R
+++ Functions/Simulations_Chapter4.R
@@ -628,3 +628,60 @@ SummarizeTable <- function(my.two.by.two.table) {
                    "TPR"=TPR,
                    "FPR"=FPR))
 }
+GetComparisonRules <- function(scenario,
+                                     B.optimal,
+                                     test.df,
+                                     study.design,
+                                     name.outcome, type.outcome, desirable.outcome,
+                                     name.treatment, names.influencing.treatment, names.influencing.rule,
+                                     propensity.method,
+                                     clinical.threshold=0, 
+                                     bootstrap.CI=FALSE) {
+    # optimal rule
+    evaluate.optimal.rule <- EvaluateRule(data=test.df,
+                                      BuildRule.object=NULL,
+                                      B=B.optimal,
+                                      study.design=study.design,
+                                      name.outcome=name.outcome,
+                                      type.outcome=type.outcome,
+                                      desirable.outcome=desirable.outcome,
+                                      clinical.threshold=0, 
+                                      name.treatment=name.treatment,
+                                      names.influencing.treatment=names.influencing.treatment,
+                                      names.influencing.rule=names.influencing.rule,
+                                      propensity.method=propensity.method,
+                                      bootstrap.CI=bootstrap.CI)
+    # treating all
+    B.treat.all <- rep(1, nrow(test.df))
+    evaluate.treat.all <- EvaluateRule(data=test.df,
+                                        BuildRule.object=NULL,
+                                        B=B.treat.all,
+                                        study.design=study.design,
+                                        name.outcome=name.outcome,
+                                        type.outcome=type.outcome,
+                                        desirable.outcome=desirable.outcome,
+                                        clinical.threshold=0, 
+                                        name.treatment=name.treatment,
+                                        names.influencing.treatment=names.influencing.treatment,
+                                        names.influencing.rule=names.influencing.rule,
+                                        propensity.method=propensity.method,
+                                        bootstrap.CI=bootstrap.CI)
+    # treating none
+    B.treat.none <- rep(0, nrow(test.df))
+    evaluate.treat.none <- EvaluateRule(data=test.df,
+                                        BuildRule.object=NULL,
+                                        B=B.treat.none,
+                                        study.design=study.design,
+                                        name.outcome=name.outcome,
+                                        type.outcome=type.outcome,
+                                        desirable.outcome=desirable.outcome,
+                                        clinical.threshold=0, 
+                                        name.treatment=name.treatment,
+                                        names.influencing.treatment=names.influencing.treatment,
+                                        names.influencing.rule=names.influencing.rule,
+                                        propensity.method=propensity.method,
+                                        bootstrap.CI=bootstrap.CI)
+    return(list("evaluate.optimal.rule"=evaluate.optimal.rule,
+                  "evaluate.treat.all"=evaluate.treat.all,
+                   "evaluate.treat.none"=evaluate.treat.none))
+}
